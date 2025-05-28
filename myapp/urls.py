857ac836bced16d1b066260c4e985a1aa01b5c1a
Julias-Ai-Developer <muyambijulias@gmail.com>
2025-05-28 04:00:16 +0300

    change the path to login

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),  # homepage,
      path('home', views.home, name='home'), 
+     path('login/', views.login_view, name='login'), 
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('forgotpassword/', views.forgotpassword_view, name='forgotpassword'),
