f3e363cdc58ed8ac66699f1976cc648e0a9e9d4d
Julias-Ai-Developer <muyambijulias@gmail.com>
2025-06-10 09:47:59 +0300

    new commits

---
+++ myapp/category_views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import  Category
+
+@login_required
+def categories(request):
+        if request.method == "POST":
+              category_name = request.POST.get("categoryName")
+
+              if category_name:
+                      Category.objects.create(
+                                          category_name=category_name,
+                                          created_by=request.user,
+                            )
+              else:
+                            pass
+
+              return redirect("categories")
+        else:
+              categories = Category.objects.filter(deleted_at=1).order_by(
+              "-id"
+              )
+              return render(request, "myapp/categories.html", {"categories": categories})
+
+
+@login_required
+def edit_category(request):
+        if request.method == "POST":
+              id = request.POST.get("id")
+              category_name = request.POST.get("categoryName")
+
+              if category_name and id:
+              #         check if categoy exists for editing
+                  category_check = Category.objects.filter(id=id)
+                  if category_check.count() > 0:
+                            category = category_check.first()
+                            category.category_name = category_name
+                            category.save()
+                  else:
+                      pass
+                              
+              
+              else:
+                  pass
+
+              return redirect("categories")
+        else:
+              return redirect("categories")
+        
+@login_required
+def delete_category(request):
+        if request.method == "POST":
+              id = request.POST.get("id")
+
+              if id:
+              #         check if categoy exists for editing
+                  category_check = Category.objects.filter(id=id)
+                  if category_check.count() > 0:
+                            category = category_check.first()
+                            category.deleted_at = 0
+                            category.save()
+                  else:
+                      pass
+                              
+              
+              else:
+                  pass
+
+              return redirect("categories")
+        else:
+              return redirect("categories")
+
+             
